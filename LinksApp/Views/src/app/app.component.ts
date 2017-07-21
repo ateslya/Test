a750870ead9ab7d0edf2315210a4649c24876692
Антон Тесля <anton.teslya90@gmail.com>
2017-07-21 17:47:05 +0500

    Added angular

---
+++ LinksApp/Views/src/app/app.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'LinksApp/Views/src/app/app.component.html'
+})
+
+export class AppComponent {
+    URL = '';
+
+    constructor() { this.log(`constructor`); }
+
+    ngOnInit() { this.log(`onInit`); }
+
+    ngOnDestroy() { this.log(`onDestroy`); }
+
+    private log(msg: string) {
+        console.log(msg);
+    }
+
+    zipURL(URL: string) {
+        console.log(URL);
+        //this.phones.push(new Phone(title, price, company));
+    }
+}
